5692f2d05004d22e5ebed8812db76c1bfd08ea4a
welcome-2themachine <Christopher.prevost94@gmail.com>
2016-02-24 09:43:52 -0500

    Added energyLevel and methods to get, add and subtract from the energy level.

--- src/server/FieldData.java
+++ src/server/FieldData.java
@@ -14,8 +14,9 @@ import com.jme3.network.serializing.Serializable;
 // -------------------------------------------------------------------------
 @Serializable
 public class FieldData {
-
+    private final int INITIALENERGYLEVEL = 100;
     public int id;
+    private int energyLevel;
     public float x, y, z;
     public ColorRGBA color;
 
@@ -28,5 +29,40 @@ public class FieldData {
         this.y = y;
         this.z = z;
         this.color = c;
+        energyLevel = INITIALENERGYLEVEL;
+    }
+    
+    public int getEnergyLevel(){
+        return energyLevel;
+    }
+    
+    public boolean addEnergy(int add){
+        if(energyLevel > 0){
+            energyLevel += add;
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    
+    /**
+     * @param sub
+     * @return -1 if true, 0 if false
+     */
+    public int subtractEnergy(int sub){
+        if(energyLevel > 0){
+            if(energyLevel - sub > 0){
+                energyLevel -= sub;
+            }
+            else{
+                energyLevel = 0;
+                return (sub - energyLevel);
+            }
+            return -1;
+        }
+        else{
+            return 0;
+        }
     }
 }
