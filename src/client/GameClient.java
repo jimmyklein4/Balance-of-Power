6d3ab783da7ad44caf7a4ed6c76eab400bc94608
Jimmy Klein <james.klein@temple.edu>
2016-02-25 17:28:55 -0500

    Added way to see waht you're clicking on

--- src/client/GameClient.java
+++ src/client/GameClient.java
@@ -4,6 +4,7 @@ import com.jme3.app.DebugKeysAppState;
 import com.jme3.input.MouseInput;
 import com.jme3.app.SimpleApplication;
 import com.jme3.app.StatsAppState;
+import com.jme3.collision.CollisionResults;
 import com.jme3.input.KeyInput;
 import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.KeyTrigger;
@@ -11,6 +12,8 @@ import com.jme3.input.controls.MouseButtonTrigger;
 import com.jme3.light.AmbientLight;
 import com.jme3.light.DirectionalLight;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.Ray;
+import com.jme3.math.Vector2f;
 import com.jme3.math.Vector3f;
 import com.jme3.network.Message;
 import com.jme3.post.FilterPostProcessor;
@@ -161,6 +164,21 @@ public class GameClient extends SimpleApplication implements ClientNetworkListen
     // key action
     public void onAction(String name, boolean isPressed, float tpf) {
         if (isPressed) {
+            if(name.equals("Select")){
+                CollisionResults results = new CollisionResults();
+                Vector2f click2d = inputManager.getCursorPosition();
+                Vector3f click3d = cam.getWorldCoordinates(new Vector2f(click2d.x, click2d.y),0f).clone();
+                Vector3f dir = cam.getWorldCoordinates(new Vector2f(click2d.x, click2d.y), 1f).subtractLocal(click3d).normalizeLocal();
+                
+                Ray ray = new Ray(click3d, dir);
+                rootNode.collideWith(ray, results);
+                for(int i=0; i<results.size(); i++){
+                    float dist = results.getCollision(i).getDistance();
+                    Vector3f pt = results.getCollision(i).getContactPoint();
+                    String target = results.getCollision(i).getGeometry().getName();
+                    System.out.println("Selection #" + i + ": " + target + " at " + pt + ", " + dist + " WU away.");
+                }
+            }
         }
     }
     
