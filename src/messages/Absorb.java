74e50bb8110e6f9ea8abfa68fcbdc5555c594f43
TheoHauser <theo.hauser@temple.edu>
2016-02-25 12:48:16 -0500

    "added absorb message class"

---
+++ src/messages/Absorb.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package messages;
+
+/**
+ *
+ * @author Theo
+ */
+public class Absorb {
+    public int sender;
+    public int reciever;
+    
+    public Absorb(){
+        
+    }
+    public Absorb(int sender, int reciever){
+        this.sender = sender;
+        this.reciever = reciever;
+    }
+}
